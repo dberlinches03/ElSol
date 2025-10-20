e70c7479118de3ba7dcd499af8ae29dd5f6dad94
davberamo <dberlinches2003@gmail.com>
2025-10-20 20:41:12 +0200

    Modificado el Ejercicio 2 del Sol

--- app/src/main/java/com/example/elsol/MainActivity.kt
+++ app/src/main/java/com/example/elsol/MainActivity.kt
@@ -5,16 +5,27 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.recyclerview.widget.GridLayoutManager
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
+
+    private val solarItems = mutableListOf(
+        SolarItem("Corona solar", R.drawable.corona_solar),
+        SolarItem("Erupción solar", R.drawable.erupcionsolar),
+        SolarItem("Espículas", R.drawable.espiculas),
+        SolarItem("Filamentos", R.drawable.filamentos),
+        SolarItem("Magnetosfera", R.drawable.magnetosfera),
+        SolarItem("Mancha solar", R.drawable.manchasolar)
+    )
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+        val recyclerView = findViewById<RecyclerView>(R.id.solarRecyclerView)
+        recyclerView.layoutManager = GridLayoutManager(this, 2)
+        recyclerView.adapter = SolarAdapter(solarItems)
     }
 }
